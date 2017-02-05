9ec8eaa0a5789f2a998ce641b39896d49af172ad
Ryan Kahle <jrkahle@hotmail.com>
2017-02-05 09:13:46 -0500

    adds functionality to get input

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,13 +1,45 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class Form extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      search: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange (e) {
+    this.setState({
+      search: e.target.value
+    })
+    console.log(this.state.search)
+  }
+  handleSubmit (e) {
+    e.preventDefault()
+    var search = this.state.search
+    // need to do something with search results
+    // refactor and move logic to FormContainer
+  }
   render () {
     return (
-      <form className={this.props.passClass} style={this.props.passStyle}>
+      <form
+        className={this.props.passClass}
+        style={this.props.passStyle}
+        onSubmit={this.handleSubmit}
+      >
         <div className='form-group'>
-          <input className='form-control' placeholder='Washington, DC' style={this.props.styleInput} />
+          <input
+            className='form-control'
+            placeholder='Washington, DC'
+            style={this.props.styleInput}
+            onChange={this.handleChange}
+          />
         </div>
-        <button className='btn btn-success' style={this.props.styleButton}>Get Weather</button>
+        <button
+          className='btn btn-success'
+          style={this.props.styleButton}
+        >Get Weather</button>
       </form>
     )
   }
